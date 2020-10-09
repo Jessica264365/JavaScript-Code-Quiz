9800a65ea90642e46dbab5e961cab596b5c53f05
Jessica Whitman <oprah135@gmail.com>
2020-10-08 19:30:13 -0700

    updating questions

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let randomQues;
 let currentQuestionIndex;
 const questionEl = document.getElementById("question");
 const answerBtnEl = document.getElementById("answer-btns");
-let timerStart = parseInt(75);
+let timerStart = parseInt(46);
 let timerInterval;
 let timer = document.getElementById("timer");
 let scoreForm = document.getElementById("scoreForm");
@@ -16,38 +16,39 @@ let userScore = document.getElementById("userScore");
 let highScoreEl = document.getElementById("highscores");
 let highscoreListEl = document.getElementById("scoreList");
 let highScoreLink = document.getElementById("highScoreList");
+let nav = document.getElementById("nav");
 
 // Questions for Quiz
 let questions = [
   {
-    question: "What is 2+2?",
+    question: "What does () => stand for in JavaScript",
     answers: [
-      { text: "4", correct: true },
-      { text: "34", correct: false },
-      { text: "60", correct: false },
-      { text: "15", correct: false },
+      { text: "A function", correct: true },
+      { text: "A boolean", correct: false },
+      { text: "An if statement", correct: false },
+      { text: "An empty array", correct: false },
     ],
   },
   {
-    question: "What is 5+5?",
+    question: "Which is NOT an event listener?",
     answers: [
-      { text: "10", correct: true },
-      { text: "60", correct: false },
-      { text: "4", correct: false },
-      { text: "15", correct: false },
+      { text: "mouseover", correct: false },
+      { text: "click", correct: false },
+      { text: "keyclick", correct: true },
+      { text: "submit", correct: false },
     ],
   },
   {
-    question: "What is 30+5?",
+    question: "What is sometimes used as a shortcut for an if statement?",
     answers: [
-      { text: "24", correct: false },
-      { text: "35", correct: true },
-      { text: "90", correct: false },
-      { text: "7", correct: false },
+      { text: "A ternary operator", correct: true },
+      { text: "The spread operator", correct: false },
+      { text: "There isn't one", correct: false },
+      { text: "object literal notation", correct: false },
     ],
   },
   {
-    question: "What is 1+2?",
+    question: "",
     answers: [
       { text: "3", correct: true },
       { text: "89", correct: false },
@@ -69,8 +70,6 @@ let questions = [
 // Event listeners for startBtn
 startBtn.addEventListener("click", startQuiz);
 startBtn.addEventListener("click", startTimer);
-
-// Event listener for Highscores
 
 // Timer is started when startBtn is clicked
 function startTimer() {
@@ -94,9 +93,7 @@ function startTimer() {
 function startQuiz() {
   startBtn.classList.add("hide");
   // Questions are randomly chosen
-  randomQues = questions.sort(function () {
-    Math.random() - 0.5;
-  });
+  randomQues = questions.sort(() => Math.random() - 0.5);
   currentQuestionIndex = 0;
   quizDescript.classList.add("hide");
   questionContEl.classList.remove("hide");
@@ -110,6 +107,7 @@ function nextQuestion() {
   displayQues(randomQues[currentQuestionIndex]);
 }
 
+// Buttons are made for each possible answer and the question is displayed
 function displayQues(question) {
   questionEl.innerText = question.question;
   question.answers.forEach(function (answer) {
@@ -123,20 +121,23 @@ function displayQues(question) {
     answerBtnEl.appendChild(button);
   });
 }
+
+// Buttons are reset for the next question
 function resetBtn() {
   while (answerBtnEl.firstChild) {
     answerBtnEl.removeChild(answerBtnEl.firstChild);
   }
 }
 
+// Answer is selected. If it's correct the timer stays the same
 function selectedAns(event) {
   let selectedAns = event.target;
+  // If the answer is wrong 10 seconds is deducted from the timer
   let rightAns = selectedAns.dataset.correct;
   if (!rightAns) {
     timerStart = timerStart - 10;
   }
 
-  console.log(answerBtnEl.children);
   Array.from(answerBtnEl.children).forEach(function (button) {
     btnClass(button, button.dataset.correct);
   });
@@ -145,7 +146,7 @@ function selectedAns(event) {
     currentQuestionIndex++;
     setTimeout(() => {
       nextQuestion();
-    }, 800);
+    }, 500);
   } else if (
     confirm("Game over! Would you like to record your score?") === true
   ) {
@@ -153,20 +154,23 @@ function selectedAns(event) {
     questionContEl.classList.add("hide");
     clearInterval(timerInterval);
 
+    // When all the questions are answered the user is prompted to record their score
+
     submitBtn.addEventListener("click", function (event) {
       event.preventDefault();
       highScoreEl.classList.remove("hide");
       scoreForm.classList.add("hide");
+      nav.classList.add("hide");
 
       let finalScore = {
         user: userScore.value.trim(),
         score: timerStart,
       };
-
+      // if the user does not put in a name an alert is displayed
       if (finalScore === "") {
         alert("You must enter a name!");
       }
-
+      // The scores are saved in local storage
       let scoreArray = localStorage.getItem("finalScore")
         ? JSON.parse(localStorage.getItem("finalScore"))
         : [];
@@ -179,10 +183,11 @@ function selectedAns(event) {
         li.textContent = scoreArray.user + ":  " + scoreArray.score;
         highscoreListEl.appendChild(li);
       };
-
+      // A list is made from the stored scores
       showScores.forEach((scoreArray) => {
         highScoreListMaker(scoreArray);
       });
+      // A restart button for the quiz is added
       let restartQuizBtn = document.createElement("button");
       highScoreEl.appendChild(restartQuizBtn);
       restartQuizBtn.textContent = "Restart Quiz";
@@ -190,15 +195,18 @@ function selectedAns(event) {
         location.reload("./quiz.html");
       });
     });
+  } else {
+    location.reload("./quiz.html");
   }
 }
-
+// If the link to "Highscores" is clicked, the highscores are displayed to the user
 highScoreLink.addEventListener("click", () => {
   highScoreEl.classList.remove("hide");
   startBtn.classList.add("hide");
   quizDescript.classList.add("hide");
   questionContEl.classList.add("hide");
   answerBtnEl.classList.add("hide");
+  nav.classList.add("hide");
 
   clearInterval(timerInterval);
   let scoreArray = localStorage.getItem("finalScore")
@@ -214,6 +222,7 @@ highScoreLink.addEventListener("click", () => {
   showScores.forEach((scoreArray) => {
     highScoreListMaker(scoreArray);
   });
+  // A button is created for the user to return to the quiz
   let returnToQuizBtn = document.createElement("button");
   highScoreEl.appendChild(returnToQuizBtn);
   returnToQuizBtn.textContent = "Return to Quiz";
@@ -222,6 +231,7 @@ highScoreLink.addEventListener("click", () => {
   });
 });
 
+// A class is added depending on if the answer was correct or wrong
 function btnClass(element, correct) {
   clearBtnClass(element);
   if (correct) {
@@ -230,7 +240,7 @@ function btnClass(element, correct) {
     element.classList.add("wrong");
   }
 }
-
+// The classes are cleared for the next set of possible answers
 function clearBtnClass(element) {
   element.classList.remove("correct");
   element.classList.remove("wrong");
